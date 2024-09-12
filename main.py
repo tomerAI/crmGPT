0cba1f6a430d3d9b9d321cad2334d3efbb13f2ce
tomerAI <thhjnissen@gmail.com>
2024-09-12 20:44:31 +0200

    test

---
+++ main.py
@@ -0,0 +1,8 @@
+import getpass
+import os
+from dotenv import load_dotenv
+
+
+# set API keys for OpenAI (the LLM we will use) and Tavily (the search tool we will use)
+# Load environment variables from .env file
+load_dotenv()
